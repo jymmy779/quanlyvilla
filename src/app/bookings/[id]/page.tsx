a5ee2133852fa6fca3ee8b7e5e3d29e01186c10a
thaigithurb <jymmynguyen103@gmail.com>
2026-05-27 09:19:41 +0700

    Update page.tsx

--- src/app/bookings/[id]/page.tsx
+++ src/app/bookings/[id]/page.tsx
@@ -448,7 +448,7 @@ Cám ơn quý khách 🌸`;
                       <button
                         onClick={() => updateStatus('checked_in')}
                         disabled={updating || isCheckInDisabled}
-                        className={`w-full md:w-auto bg-indigo-600 text-white px-5 md:px-6 py-2 md:py-2.5 rounded-xl font-semibold text-sm shadow-md transition-all ${isCheckInDisabled ? 'opacity-30 grayscale cursor-not-allowed' : 'hover:bg-indigo-700 active:scale-95'}`}
+                        className={`w-full md:w-auto bg-indigo-600 text-white px-5 md:px-6 text-nowrap py-2 md:py-2.5 rounded-xl font-semibold text-sm shadow-md transition-all ${isCheckInDisabled ? 'opacity-30 grayscale cursor-not-allowed' : 'hover:bg-indigo-700 active:scale-95'}`}
                       >
                         {isCheckInDisabled ? 'Chưa tới ngày' : 'Check-in ngay'}
                       </button>
